7c40268f6866746861e60e9d288d438f1e8968c1
Yashvardhan <iamyashvardhan6@gmail.com>
2024-03-30 09:45:16 +0530

    update pythonProcess in upload api

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,5 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { parseForm, FormidableError } from "../../lib/parse-form";
+import { exec, spawn } from 'child_process';
+
+async function executePythonScript(scriptPath: string, args: string[]) {
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn('python3', [scriptPath, ...args]);
+
+    let stdoutData = '';
+    let stderrData = '';
+
+    pythonProcess.stdout.on('data', (data) => {
+      stdoutData += data.toString();
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+      stderrData += data.toString();
+    });
+
+    pythonProcess.on('close', (code) => {
+      if (code === 0) {
+        resolve(stdoutData);
+      } else {
+        reject(new Error(`Python script exited with code ${code}. stderr: ${stderrData}`));
+      }
+    });
+
+    pythonProcess.on('error', (error) => {
+      reject(error);
+    });
+  });
+}
 
 const handler = async (
 	req: NextApiRequest,
@@ -29,11 +59,38 @@ const handler = async (
 			let url = Array.isArray(file) ? file.map((f) => f.filepath) : file.filepath;
 			urls.push(String(url));
 		})
-		// const file = files.media;
-		// let url = Array.isArray(file) ? file.map((f) => f.filepath) : file.filepath;
+
+		const scriptPath = `inference.py`;
+
+		const args = [`${urls[0]}`, './uploads/30-03-2024/converted.mp4'];
+
+		try {
+			const output = await executePythonScript(scriptPath, args);
+			console.log('Python script output:', output);
+		} catch (error) {
+			console.error('Error executing Python script:', error);
+		}
+	
+
+		// chmod +x script.py
+
+		// exec(`chmod +x ${urls[0]}`, (error, stdout, stderr) => {
+		// 	if (error) {
+		// 		console.error(`chmod error : ${error}`);
+		// 		return;
+		// 	}
+		// 	console.log(`chmod output: ${stdout}`);
+		// });
 
 
-		console.log("url",urls)
+		// exec(`python3 "${scriptPath}"`, (error, stdout, stderr) => {
+		// 	if (error) {
+		// 		console.error(`Error executing Python script: ${error}`);
+		// 		return;
+		// 	}
+		// 	console.log(`Python script output: ${stdout}`);
+		// });
+
 		res.status(200).json({
 			data: {
 				url: urls,
